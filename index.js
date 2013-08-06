78f34e4a961bb5c875167783d735bed9f3e5c822
Jason Campbell <jason@js.la>
2013-08-06 16:54:21 -0700

    Refactor, allows for nested imports

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
 
+var colors = require('colors')
+
 module.exports = function(cwd){
   var path = require('path')
-  var fs = require('graceful-fs')
-  var css = require('css')
+    , fs = require('graceful-fs')
+    , css = require('css')
 
   cwd = path.resolve(cwd)
 
-  return importer
-
-  function importer(style, rework){
-    var rules = rework.obj.stylesheet.rules
-    var imports = style.rules.map(map).filter(filter)
-    var read = []
-
-    // expand the media queries?
-
-    imports.forEach(function(filename){
-      if (read.indexOf(filename) >= 0) return
-
-      var data = fs.readFileSync(filename, 'utf8')
-      rules = css.parse(data).stylesheet.rules.concat(rules)
-      read.push(filename)
-    })
-
-    rework.obj.stylesheet.rules = rules
+  return function(style){
+    expand(style)
   }
 
-  function map(rule, index, array){
-    // only care about imports
-    if (rule.type !== 'import') return
+  // checks for imports and reads/ exands them into the style.rules
+  function expand(style){
+    var rules = []
 
+    for (var i = 0; i < style.rules.length; i++){
+      var rule = style.rules[i]
+        , isNotImport = rule.type !== 'import'
+
+      if (isNotImport || !filename(rule.import)) {
+        rules.push(rule)
+        continue
+      }
+
+      var expanded = scan(filename(rule.import))
+      rules = rules.concat(expanded.rules)
+    }
+
+    style.rules = rules
+
+    if (hasImports(style)) expand(style)
+  }
+
+  function hasImports(style){
+    var imports = style
+        .rules
+        .filter(function(rule){
+          return rule.type === 'import'
+        })
+
+    return !! imports.length
+  }
+
+  function scan(filename){
+    var data = fs.readFileSync(filename, 'utf8')
+    var style = css.parse(data).stylesheet
+
+    return style
+  }
+
+  function filename(id){
     // ignore imports that are urls
-    if (/^\s*http/.test(filename)) return
+    if (/^\s*http/.test(id)) return
 
-    var filename = rule.import
+    var file = id
         // extract from url(<filename>)
         .replace(/^url\(/, '')
         .replace(/\)$/, '')
@@ -42,12 +64,11 @@ module.exports = function(cwd){
         .replace(/("|\')$/, '')
 
     // append .css if it's missing
-    if (path.extname(filename) === '') filename += '.css'
+    if (path.extname(file) === '') file += '.css'
 
-    return path.resolve(cwd, filename)
-  }
+    // resolve against the cwd
+    file = path.resolve(cwd, file)
 
-  function filter(filename, index, array){
-    return !! filename
+    return file
   }
 }
