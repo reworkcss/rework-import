529e083cac78b9474159e1e864b7809552200e61
Kevin Martensson <kevinmartensson@gmail.com>
2013-09-11 17:21:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# rework-inline
+
+Inline stylesheets using `@import`.
+
+## Getting started
+
+Install with [npm](https://npmjs.org/package/rework-inline): `npm install rework-inline`
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License) (c) [Kevin Mårtensson](http://kevinmartensson.com)
---
+++ inline.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var css = require('css');
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Inline stylesheets using `@import`
+ */
+
+module.exports = function (cwd) {
+    if (!Array.isArray(cwd)) {
+        cwd = Array.prototype.slice.call(arguments);
+    }
+
+    function check(filename) {
+        filename = filename
+            .replace(/^url\s?\(/, '')
+            .replace(/\)$/, '')
+            .replace(/^("|\')/, '')
+            .replace(/("|\')$/, '');
+
+        var file = cwd.map(function (dir) {
+            return path.join(dir, filename);
+        }).filter(fs.existsSync)[0];
+
+        if (!file) {
+            throw new Error('failed to find ' + filename);
+        }
+
+        return file;
+    }
+
+    function read(file) {
+        var data = fs.readFileSync(file);
+        var style = css.parse(data).stylesheet;
+
+        return style;
+    }
+
+    return function (style) {
+        var rules = [];
+
+        style.rules.forEach(function (rule) {
+            if (rule.type !== 'import') {
+                return rules.push(rule);
+            }
+
+            var re = /(url\s?\()?(\'|")(.*)(\'|")(\))?/g;
+            var imprt = rule.import.match(re).toString();
+            var media = rule.import.replace(re, '').replace(' ', '');
+            var file = check(imprt);
+            var data = read(file);
+
+            rules = rules.concat(data.rules);
+        });
+
+        style.rules = rules;
+    };
+};
