41802ad2a36aeafc19463cf987d783d69b074baa
Kevin Martensson <kevinmartensson@gmail.com>
2013-09-13 08:15:39 +0200

    Code style

--- inline.js
+++ inline.js
@@ -5,39 +5,57 @@ var fs = require('fs');
 var path = require('path');
 
 /**
- * Inline stylesheets using `@import`
+ * Read the contents of a stylesheet
+ *
+ * @param {String} file
+ * @api private
  */
 
-module.exports = function (cwd) {
+function read(file) {
+    var data = fs.readFileSync(file, 'utf8');
+    var style = css.parse(data).stylesheet;
+
+    return style;
+}
+
+/**
+ * Check if a file exists
+ *
+ * @param {String} filename
+ * @param {String|Array} cwd
+ * @api private
+ */
+
+function check(filename, cwd) {
     if (!Array.isArray(cwd)) {
         cwd = Array.prototype.slice.call(arguments);
     }
 
-    function check(filename) {
-        filename = filename
-            .replace(/^url\s?\(/, '')
-            .replace(/\)$/, '')
-            .replace(/^("|\')/, '')
-            .replace(/("|\')$/, '');
+    filename = filename
+        .replace(/^url\s?\(/, '')
+        .replace(/\)$/, '')
+        .replace(/^("|\')/, '')
+        .replace(/("|\')$/, '');
 
-        var file = cwd.map(function (dir) {
-            return path.join(dir, filename);
-        }).filter(fs.existsSync)[0];
+    var file = cwd.map(function (dir) {
+        return path.join(dir, filename);
+    }).filter(fs.existsSync)[0];
 
-        if (!file) {
-            throw new Error('failed to find ' + filename);
-        }
-
-        return file;
+    if (!file) {
+        throw new Error('failed to find ' + filename);
     }
 
-    function read(file) {
-        var data = fs.readFileSync(file, 'utf8');
-        var style = css.parse(data).stylesheet;
+    return file;
+}
 
-        return style;
-    }
+/**
+ * Inline stylesheet using `@import`
+ *
+ * @param {String|Array} cwd
+ * @api public
+ */
 
+module.exports = function (cwd) {
     return function (style) {
         var rules = [];
 
@@ -49,7 +67,7 @@ module.exports = function (cwd) {
             var re = /(url\s?\()?(\'|")(.*)(\'|")(\))?/g;
             var imprt = rule.import.match(re).toString();
             var media = rule.import.replace(re, '').replace(' ', '');
-            var file = check(imprt);
+            var file = check(imprt, cwd);
             var data = read(file);
             var res;
 
